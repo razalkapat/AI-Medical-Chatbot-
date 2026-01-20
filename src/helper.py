1f7b94db24b3821b42c72057282905c9c11a9e48
Mohamed Razal K <razalkapat@gmail.com>
2026-01-21 00:17:53 +0530

    helper.py fixed

--- src/helper.py
+++ src/helper.py
@@ -1,20 +1,21 @@
-from langchain.document_loaders import PyPDFLoader, DirectoryLoader
+
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import HuggingFaceEmbeddings
 from typing import List
 from langchain.schema import Document
+from langchain_community.document_loaders import DirectoryLoader, PyPDFLoader
 
 
-#Extract Data From the PDF File
+# Extract Data From the PDF File
 def load_pdf_file(data):
-    loader= DirectoryLoader(data,
-                            glob="*.pdf",
-                            loader_cls=PyPDFLoader)
+    loader = DirectoryLoader(
+        data,
+        glob="*.pdf",
+        loader_cls=PyPDFLoader
+    )
 
-    documents=loader.load()
-
+    documents = loader.load()
     return documents
-
 
 
 def filter_to_minimal_docs(docs: List[Document]) -> List[Document]:
@@ -23,6 +24,7 @@ def filter_to_minimal_docs(docs: List[Document]) -> List[Document]:
     containing only 'source' in metadata and the original page_content.
     """
     minimal_docs: List[Document] = []
+
     for doc in docs:
         src = doc.metadata.get("source")
         minimal_docs.append(
@@ -31,7 +33,9 @@ def filter_to_minimal_docs(docs: List[Document]) -> List[Document]:
                 metadata={"source": src}
             )
         )
+
     return minimal_docs
+
 
 
 
